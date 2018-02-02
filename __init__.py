2ea3bce2ba70f0fd7f10f1ef031a3105a1386cae
C?dric Krier <ced@b2ck.com>
2018-02-02 17:21:19 +0100

    Update to 4.7

--- __init__.py
+++ __init__.py
@@ -1,12 +1,13 @@
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
 from trytond.pool import Pool
-from .product import *
+from . import product
+
 
 def register():
     Pool.register(
-        PriceListCategory,
-        Template,
-        PriceList,
-        PriceListLine,
+        product.PriceListCategory,
+        product.Template,
+        product.PriceList,
+        product.PriceListLine,
         module='product_price_list_price_category', type_='model')
--- tests/__init__.py
+++ tests/__init__.py
@@ -1,3 +1,8 @@
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
-from .test_product_price_list_price_category import suite
+try:
+    from trytond.modules.product_price_list_price_category.tests.test_product_price_list_price_category import suite
+except ImportError:
+    from .test_product_price_list_price_category import suite
+
+__all__ = ['suite']
